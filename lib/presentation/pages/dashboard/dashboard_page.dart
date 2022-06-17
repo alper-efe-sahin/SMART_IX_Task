53f874f749270ae468619f1aa56c18b0741c30a7
Alper Efe Şahin <efe.mate@gmail.com>
2022-06-17 17:02:48 +0300

    add exit button in the Dashboard page

--- lib/presentation/pages/dashboard/dashboard_page.dart
+++ lib/presentation/pages/dashboard/dashboard_page.dart
@@ -1,18 +1,31 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:smart_ix_task/presentation/common_widgets/colors.dart';
+import 'package:smart_ix_task/presentation/common_widgets/custom_app_bar.dart';
 import 'package:smart_ix_task/presentation/common_widgets/smart_item.dart';
+import 'package:smart_ix_task/presentation/pages/navigator/constants/texts.dart';
+import 'package:smart_ix_task/presentation/providers/firebase/firebase_provider.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends ConsumerWidget {
   const DashboardPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: 5,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemBuilder: (context, index) {
-        return const SmartItem();
-      },
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: CustomAppBar(
+        appBarTitle: dashboardText,
+        appBarAction: Icons.exit_to_app,
+        appBarBackgroundColor: transparentColor,
+        appBarIconColor: customIndigoBackgroundColor,
+        actionsOnPressed: () => ref.read(authRepositoryProvider).signOut(),
+      ),
+      body: GridView.builder(
+        itemCount: 5,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (context, index) {
+          return const SmartItem();
+        },
+      ),
     );
   }
 }
